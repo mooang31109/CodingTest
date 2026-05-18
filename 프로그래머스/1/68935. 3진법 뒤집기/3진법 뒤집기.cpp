5ad2e19b5983d4f80d7d5784120e2cd0d99f3886
김수현 <150645986+mooang31109@users.noreply.github.com>
2026-05-18 09:24:54 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.01 ms, Memory: 4.07 MB -BaekjoonHub

---
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    vector<int> v;
+    while(n > 0)
+    {
+        v.push_back(n % 3);
+        n /= 3;
+    }
+    long long mul = 1;
+    for(int i=v.size()-1; i>=0; i--)
+    {
+        answer += v[i] * mul;
+        mul *= 3;
+    }
+    return answer;
+}
